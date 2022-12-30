9007753fa4da2cb8a0536d04ef6eb8089868a527
filip316 <110289851+filip316@users.noreply.github.com>
2022-12-30 21:23:47 +0100

    Added API route for quick search results

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Work;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// Return first five quick search results from each table ordered by similarity to the query
+Route::get('/search', function () {
+    $author = Author::search(request("query"))->get();
+    $work = Work::search(request("query"))->get();
+    $book = Book::search(request("query"))->get();
+    foreach ($author as $i => $item) {
+        similar_text(strtolower($author[$i]['name']), strtolower(request('query')), $percent);
+        $author[$i]['similarity'] = $percent;
+    }
+    foreach ($work as $i => $item) {
+        similar_text(strtolower($work[$i]['title']), strtolower(request('query')), $percent);
+        $work[$i]['similarity'] = $percent;
+    }
+    foreach ($book as $i => $item) {
+        similar_text(strtolower($book[$i]['title']), strtolower(request('query')), $percent);
+        $book[$i]['similarity'] = $percent;
+    }
+    return [
+        'author' => $author->sortByDesc('similarity')->values()->take(5),
+        'work' => $work->sortByDesc('similarity')->values()->take(5),
+        'book' => $book->sortByDesc('similarity')->values()->take(5)
+    ];
 });
