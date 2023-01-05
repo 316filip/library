1e6fa459369973c6b1238a44b35c97c848f2547d
filip316 <110289851+filip316@users.noreply.github.com>
2023-01-05 18:03:28 +0100

    Changed authors table structure (again)

--- database/migrations/2022_12_21_165640_create_authors_table.php
+++ database/migrations/2022_12_21_165640_create_authors_table.php
@@ -15,7 +15,11 @@ return new class extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string("name_prefix");
+            $table->string("first_name");
+            $table->string("middle_name");
+            $table->string("last_name");
+            $table->string("name_suffix");
             $table->dateTime('birth_date');
             $table->dateTime('death_date');
             $table->longText('description');
