6978f825a89d452b213d18f2f28e95e4ab43ad99
filip316 <110289851+filip316@users.noreply.github.com>
2023-01-11 19:22:17 +0100

    Added author image to database

--- database/migrations/2022_12_21_165640_create_authors_table.php
+++ database/migrations/2022_12_21_165640_create_authors_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->dateTime('birth_date');
             $table->dateTime('death_date');
             $table->longText('description');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
