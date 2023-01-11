7ff56075e3a0a82f26e2f8e12872dcfc081cb12b
filip316 <110289851+filip316@users.noreply.github.com>
2023-01-11 19:20:24 +0100

    Made use of the search helper

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Helpers\SearchHelper;
 use App\Models\Book;
 use App\Models\Work;
 use App\Models\Author;
@@ -25,35 +26,5 @@ use Illuminate\Support\Facades\Route;
 
 // Return first five quick search results from each table ordered by similarity to the query
 Route::get('/search', function () {
-    $request = "%" . str_replace(" ", "%", request("query")) . "%";
-
-    $author_query = Author::query();
-    $author_query->where(DB::raw('concat(name_prefix, " ", first_name, " ", middle_name, " ", last_name, " ", name_suffix)'), 'like', $request);
-    $author = $author_query->get();
-    foreach ($author as $i => $item) {
-        similar_text(strtolower($author[$i]['name']), strtolower(request('query')), $percent);
-        $author[$i]['similarity'] = $percent;
-    }
-
-    $work_query = Work::query();
-    $work_query->where(DB::raw('concat(title, " ", subtitle, " ", description)'), 'like', $request);
-    $work = $work_query->get();
-    foreach ($work as $i => $item) {
-        similar_text(strtolower($work[$i]['title']), strtolower(request('query')), $percent);
-        $work[$i]['similarity'] = $percent;
-    }
-
-    $book_query = Book::query();
-    $book_query->where(DB::raw('concat(title, " ", subtitle, " ", description)'), 'like', $request);
-    $book = $book_query->get();
-    foreach ($book as $i => $item) {
-        similar_text(strtolower($book[$i]['title']), strtolower(request('query')), $percent);
-        $book[$i]['similarity'] = $percent;
-    }
-
-    return [
-        'author' => $author->sortByDesc('similarity')->values()->take(5),
-        'work' => $work->sortByDesc('similarity')->values()->take(5),
-        'book' => $book->sortByDesc('similarity')->values()->take(5)
-    ];
+    return SearchHelper::search();
 });
