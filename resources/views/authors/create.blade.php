c4c17171881081a59ed55cb6bcf95e4758e56771
filip316 <110289851+filip316@users.noreply.github.com>
2023-01-28 21:22:24 +0100

    Added create form for authors

---
+++ resources/views/authors/create.blade.php
@@ -0,0 +1,101 @@
+<x-layout>
+    <h1 class="text-4xl text-center font-bold mb-5">Přidat autora</h1>
+    <div class="flex justify-center">
+        <form method="POST" action="/autor" enctype="multipart/form-data" class="w-full px-2 max-w-2xl 2xl:max-w-3xl">
+            @csrf
+            <div class="grid grid-cols-3 sm:grid-cols-8 gap-x-4">
+                <div class="mb-3">
+                    <label for="create-author-name_prefix" class="block mb-1">Titul</label>
+                    <input type="text" class="p-2 w-full border border-slate-200 rounded-lg" name="name_prefix"
+                        id="create-author-name_prefix" placeholder="Sir" value="{{ old('name_prefix') }}"
+                        autocomplete="off">
+                    @error('name_prefix')
+                        <p class="text-red-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-3 col-span-2">
+                    <label for="create-author-first_name"
+                        class="block mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Jméno</label>
+                    <input type="text" class="p-2 w-full border border-slate-200 rounded-lg" name="first_name"
+                        id="create-author-first_name" placeholder="Arthur" value="{{ old('first_name') }}"
+                        autocomplete="off">
+                    @error('first_name')
+                        <p class="text-red-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-3 col-span-3 sm:col-span-2">
+                    <label for="create-author-middle_name" class="block mb-1">Prostřední jména</label>
+                    <input type="text" class="p-2 w-full border border-slate-200 rounded-lg" name="middle_name"
+                        id="create-author-middle_names" placeholder="Conan" value="{{ old('middle_name') }}"
+                        autocomplete="off">
+                    @error('middle_name')
+                        <p class="text-red-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-3 col-span-2">
+                    <label for="create-author-last_name"
+                        class="block mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Příjmení</label>
+                    <input type="text" class="p-2 w-full border border-slate-200 rounded-lg" name="last_name"
+                        id="create-author-last_name" placeholder="Doyle" value="{{ old('last_name') }}"
+                        autocomplete="off">
+                    @error('last_name')
+                        <p class="text-red-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="create-work-name_suffix" class="block mb-1">Titul</label>
+                    <input type="text" class="p-2 w-full border border-slate-200 rounded-lg" name="name_suffix"
+                        id="create-author-name_suffix" placeholder="" value="{{ old('name_suffix') }}"
+                        autocomplete="off">
+                    @error('name_suffix')
+                        <p class="text-red-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="grid sm:grid-cols-2 sm:gap-4">
+                <div class="mb-3">
+                    <label for="create-author-birth_date" class="block mb-1">Datum narození</label>
+                    <input type="date" class="p-2 w-full border border-slate-200 rounded-lg" name="birth_date"
+                        id="create-author-birth_date" placeholder="22. 05. 1859" value="{{ old('bith_date') }}"
+                        autocomplete="off">
+                    @error('bith_date')
+                        <p class="text-red-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="create-author-death_date" class="block mb-1">Datum úmrtí</label>
+                    <input type="date" class="p-2 w-full border border-slate-200 rounded-lg" name="death_date"
+                        id="create-author-death_date" placeholder="07. 06. 1930" value="{{ old('death_date') }}"
+                        autocomplete="off">
+                    @error('death_date')
+                        <p class="text-red-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="create-work-description" class="block mb-1">Popis</label>
+                <textarea name="description" class="p-2 w-full border border-slate-200 rounded-lg" id="create-work-description"
+                    cols="30" rows="10" autocomplete="off">{{ old('description') }}</textarea>
+                @error('description')
+                    <p class="text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="create-author-image" class="block mb-1">Obrázek/Fotografie</label>
+                <input type="file"
+                    class="w-full border border-slate-200 rounded-lg file:font-sans file:border file:border-solid file:border-sky-100 file:bg-sky-200 file:shadow-sm file:px-3 file:py-2 file:mr-2 file:rounded-lg text-slate-500"
+                    name="image" id="create-author-image" autocomplete="off">
+                @error('image')
+                    <p class="text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="flex justify-center">
+                <div class="mb-3">
+                    <input type="submit"
+                        class="px-3 py-2 border border-slate-200 bg-yellow-400 hover:bg-amber-400 shadow-sm rounded-lg transition"
+                        value="Vytvořit">
+                </div>
+            </div>
+        </form>
+    </div>
+</x-layout>
