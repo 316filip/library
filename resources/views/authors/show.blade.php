4c56d4755d392bfc95bf8b49efab7aab0420b923
filip316 <110289851+filip316@users.noreply.github.com>
2023-01-29 21:15:37 +0100

    Added buttons for editing and removing author

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -1,13 +1,28 @@
 <x-layout>
     @unless($author->image === null)
         <div class="flex justify-center mb-3">
-            <div class="bg-sky-100 h-52 w-52 bg-cover bg-center rounded-full"
+            <div class="bg-sky-100 h-52 w-52 bg-cover bg-center rounded-full shadow"
                 style="background-image: url({{ asset('/img/' . $author->image) }})"></div>
         </div>
     @endunless
     <h1 class="text-4xl text-center font-bold mb-5">
         {{ $author->name }}
     </h1>
+    <div class="flex gap-3 justify-center mb-5">
+        <div class="h-8 w-8">
+            <p class="h-full w-full bg-yellow-400 rounded-full text-center">
+                <a class="align-middle" href="/autor/{{ $author->id }}/upravit">
+                    <i class="fa-regular fa-pen-to-square"></i>
+                </a>
+            </p>
+        </div>
+        <div class="h-8 w-8">
+            <button class="h-full w-full bg-red-500 rounded-full" onclick="openDelete()">
+                <i class="fa-regular fa-trash-can"></i>
+            </button>
+        </div>
+    </div>
+    <x-Delete id="{{ $author->id }}" type="author"></x-Delete>
     <h2 class="text-2xl mb-5"><a href="javascript:void(0)" onclick="authorDetails()">Informace o autorovi <i
                 class="fa-solid fa-caret-right transition rotate-90" id="author-details-toggle"></i></a></h2>
     <div id="author-details-table">
