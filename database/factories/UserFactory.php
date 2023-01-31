f3c4011927d292519afe1ff4c54220e916ac6d5b
filip316 <110289851+filip316@users.noreply.github.com>
2023-01-31 15:44:07 +0100

    Fixed admin field

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,6 @@ class UserFactory extends Factory
             'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'admin' => 0,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->boolean('admin');
+            $table->boolean('admin')->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
