9a49454fffec9ffc0130895322f518b974f55f72
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-05 19:12:45 +0100

    Improved select component

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -12,12 +12,11 @@
                 class="fa-solid fa-angle-down"></i></span>
     </div>
 
-    <div class="h-fit w-full max-w-2xl 2xl:max-w-3xl z-40 p-2" id="form-select-dropdown">
+    <div class="h-fit w-full max-w-2xl 2xl:max-w-3xl z-50 p-2" id="form-select-dropdown">
         <div class="h-full w-full rounded-md shadow-lg backdrop-blur-xl p-3" id="form-select-content"
             style="display: none">
             <input type="text" class="p-2 mb-3 w-full border border-slate-200 rounded-lg" id="form-select-filter"
-                placeholder="Prohledat {{ $search }}..." onkeyup="selectFilter()" onblur="selectClose()"
-                autocomplete="off">
+                placeholder="Prohledat {{ $search }}..." onkeyup="selectFilter()" autocomplete="off">
             <div class="h-60 overflow-auto">
                 <div id="form-select-options">
                     @if ($type == 'author')
@@ -40,26 +39,47 @@
             </div>
         </div>
     </div>
+    <div class="fixed top-0 left-0 h-full w-full hidden z-40" id="form-select-backdrop" onclick="selectClose()"></div>
 
     <script>
+        $(document)
+            .ready(function() {
+                $("#form-select-filter")
+                    .bind("keydown", "esc", function() {
+                        selectClose();
+                        return false;
+                    })
+            })
+
         Popper.createPopper(document.querySelector('#form-select'), document.querySelector(
             '#form-select-dropdown'), {
             placement: 'bottom',
         });
 
+        /**
+         * Shows options to select from
+         */
         function selectOpen() {
+            $("#form-select-backdrop").removeClass("hidden");
             $("#form-select-content").slideDown("fast");
             $("#form-select").prop("disabled", true)
             $("#form-select-filter").focus();
         }
 
+        /**
+         * Closes select dropdown
+         */
         function selectClose() {
+            $("#form-select-backdrop").addClass("hidden");
             setTimeout(() => {
                 $("#form-select").prop("disabled", false);
                 $("#form-select-content").slideUp("fast");
             }, 100);
         }
 
+        /**
+         * Filters options in select menu
+         */
         function selectFilter() {
             let empty = true;
             $("#form-select-options *").filter(function() {
@@ -78,9 +98,13 @@
             }
         }
 
+        /**
+         * Sets selected option
+         */
         function selectSet(id, name) {
             $("#form-select").val(name);
             $("#form-select-output").val(id);
+            selectClose();
         }
     </script>
 </div>
