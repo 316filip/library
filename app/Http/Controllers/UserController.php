b9564a9aeb69637f95cf66e7bcc11c561df95577
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-06 18:23:10 +0100

    Added UserHelper, user editing and deleting

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\UserHelper;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 
 class UserController extends Controller
@@ -11,15 +13,7 @@ class UserController extends Controller
     // Get one user
     public function show($user = "")
     {
-        if ($user === "") {
-            $user = auth()->user();
-        } else {
-            $user = User::where('code', $user)->first();
-
-            if ($user === null) {
-                abort(404);
-            }
-        }
+        $user = UserHelper::find($user);
 
         return view('user.show', [
             'user' => $user
@@ -89,5 +83,90 @@ class UserController extends Controller
         auth()->login($user);
 
         return redirect('/ucet')->with('message', 'Váš účet byl úspěšně vytvořen!')->with('color', 'success');
+    }
+
+    // Show edit form
+    public function edit($user = "")
+    {
+        $user = UserHelper::find($user);
+
+        return view('user.edit', [
+            'user' => $user
+        ]);
+    }
+
+    // Update user data
+    public function update(Request $request, User $user)
+    {
+        if (!auth()->user()->admin && ($user->id !== auth()->user()->id)) {
+            return redirect('/ucet')->with('message', 'Nemáte oprávnění upravovat tohoto uživatele!')->with('color', 'fail');
+        }
+
+        $link = '/ucet';
+        if ($user->id !== auth()->user()->id) {
+            $link = '/ucet/' . $user->code;
+        }
+
+        if ($request->type == 'data') {
+            $formFields = $request->validate([
+                'first_name' => ['required', 'min:3'],
+                'last_name' => ['required', 'min:3'],
+                'email' => ['required', 'email'],
+            ]);
+
+            if (count(User::where('email', $formFields['email'])->where('id',  '!=', $user->id)->get()) !== 0) {
+                return back()->withErrors(['email' => 'Tuto emailovou adresu již používá někdo jiný.']);
+            }
+
+            $user->update([
+                'first_name' => $formFields['first_name'],
+                'last_name' => $formFields['last_name'],
+                'email' => $formFields['email'],
+            ]);
+
+            return redirect($link)->with('message', 'Uživatelský profil byl úspěšně aktualizován!')->with('color', 'success');
+        } elseif ($request->type == 'password') {
+            $formFields = $request->validate([
+                'password_old' => 'required',
+                'password' => ['required', 'confirmed', 'min:6'],
+            ]);
+
+            if (!Hash::check($formFields['password_old'], $user->password)) {
+                return back()->withErrors(['password_old' => 'Chybně jste zadali současné heslo.']);
+            }
+
+            $user->update([
+                'password' => bcrypt($formFields['password']),
+            ]);
+
+            return redirect($link)->with('message', 'Heslo bylo úspěšně změněno!')->with('color', 'success');
+        } elseif ($request->type == 'competency') {
+            $formFields = $request->validate([
+                'admin' => 'nullable',
+            ]);
+
+            if (isset($formFields['admin']) && $formFields['admin'] == 1) {
+                $formFields['admin'] = true;
+            } else {
+                $formFields['admin'] = false;
+            }
+
+            $user->update([
+                'admin' => $formFields['admin'],
+            ]);
+
+            return redirect($link)->with('message', 'Oprávnění byla úspěšně použita!')->with('color', 'success');
+        }
+    }
+
+    // Delete user
+    public function destroy(User $user)
+    {
+        if (!auth()->user()->admin && ($user->id !== auth()->user()->id)) {
+            return redirect('/ucet')->with('message', 'Nemáte oprávnění odstranit tohoto uživatele!')->with('color', 'fail');
+        }
+
+        $user->delete();
+        return redirect('/')->with('message', 'Účet byl úspěšně odstraněn!')->with('color', 'success');
     }
 }
