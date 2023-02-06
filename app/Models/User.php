09a6b6345b1f56da961a90f328add6a5e3ccf534
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-06 18:28:02 +0100

    Added admin into fillable fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = ['first_name', 'last_name', 'email', 'code', 'password'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'code', 'admin', 'password'];
 
     /**
      * The attributes that should be hidden for serialization.
