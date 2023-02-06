31eb0f7d52f378f1510d09aa117e89092150e93b
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-06 18:27:05 +0100

    Added middleware and user routes

--- routes/web.php
+++ routes/web.php
@@ -43,27 +43,27 @@ Route::get('/info', function () {
 Route::get('/knihovna', [BrowseController::class, 'browse']);
 
 // AUTHOR ROUTES
-Route::get('/autor/tvorba', [AuthorController::class, 'create'])->middleware('auth');
-Route::post('/autor', [AuthorController::class, 'store'])->middleware('auth');
-Route::get('/autor/{author}/upravit', [AuthorController::class, 'edit'])->middleware('auth');
-Route::put('/autor/{author}', [AuthorController::class, 'update'])->middleware('auth');
-Route::delete('/autor/{author}', [AuthorController::class, 'destroy'])->middleware('auth');
+Route::get('/autor/tvorba', [AuthorController::class, 'create'])->middleware('admin');
+Route::post('/autor', [AuthorController::class, 'store'])->middleware('admin');
+Route::get('/autor/{author}/upravit', [AuthorController::class, 'edit'])->middleware('admin');
+Route::put('/autor/{author}', [AuthorController::class, 'update'])->middleware('admin');
+Route::delete('/autor/{author}', [AuthorController::class, 'destroy'])->middleware('admin');
 Route::get('/autor/{author}', [AuthorController::class, 'show']);
 
 // WORK ROUTES
-Route::get('/titul/tvorba', [WorkController::class, 'create'])->middleware('auth');
-Route::post('/titul', [WorkController::class, 'store'])->middleware('auth');
-Route::get('/titul/{work}/upravit', [WorkController::class, 'edit'])->middleware('auth');
-Route::put('/titul/{work}', [WorkController::class, 'update'])->middleware('auth');
-Route::delete('/titul/{work}', [WorkController::class, 'destroy'])->middleware('auth');
+Route::get('/titul/tvorba', [WorkController::class, 'create'])->middleware('admin');
+Route::post('/titul', [WorkController::class, 'store'])->middleware('admin');
+Route::get('/titul/{work}/upravit', [WorkController::class, 'edit'])->middleware('admin');
+Route::put('/titul/{work}', [WorkController::class, 'update'])->middleware('admin');
+Route::delete('/titul/{work}', [WorkController::class, 'destroy'])->middleware('admin');
 Route::get('/titul/{work}', [WorkController::class, 'show']);
 
 // BOOK ROUTES
-Route::get('/kniha/tvorba', [BookController::class, 'create'])->middleware('auth');
-Route::post('/kniha', [BookController::class, 'store'])->middleware('auth');
-Route::get('/kniha/{book}/upravit', [BookController::class, 'edit'])->middleware('auth');
-Route::put('/kniha/{book}', [BookController::class, 'update'])->middleware('auth');
-Route::delete('/kniha/{book}', [BookController::class, 'destroy'])->middleware('auth');
+Route::get('/kniha/tvorba', [BookController::class, 'create'])->middleware('admin');
+Route::post('/kniha', [BookController::class, 'store'])->middleware('admin');
+Route::get('/kniha/{book}/upravit', [BookController::class, 'edit'])->middleware('admin');
+Route::put('/kniha/{book}', [BookController::class, 'update'])->middleware('admin');
+Route::delete('/kniha/{book}', [BookController::class, 'destroy'])->middleware('admin');
 Route::get('/kniha/{book}', [BookController::class, 'show']);
 
 // USER ROUTES
@@ -73,10 +73,12 @@ Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 Route::get('/registrace', [UserController::class, 'create'])->middleware('guest');
 Route::post('/ucet', [UserController::class, 'store'])->middleware('guest');
 Route::get('/ucet', [UserController::class, 'show'])->middleware('auth');
-Route::get('/ucet/{user}', [UserController::class, 'show'])->middleware('auth');
+Route::get('/ucet/upravit', [UserController::class, 'edit'])->middleware('auth');
+Route::get('/ucet/{user}/upravit', [UserController::class, 'edit'])->middleware('admin');
+Route::get('/ucet/{user}', [UserController::class, 'show'])->middleware('admin');
+Route::put('/ucet/{user}', [UserController::class, 'update'])->middleware('auth');
+Route::delete('/ucet/{user}', [UserController::class, 'destroy'])->middleware('auth');
 
 // SEARCH ROUTES
 Route::get('/hledat', [SearchController::class, 'search']);
-Route::get('/search', function () {
-    return SearchHelper::search();
-});
+Route::get('/search', [SearchController::class, 'quick']);
