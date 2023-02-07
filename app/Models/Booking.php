1e8d9fa36c9e5d7ab9510c273f16c823e05e87c8
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-07 22:07:27 +0100

    Added fillable fields & relationship to user

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,16 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Booking extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['code', 'book_id', 'user_id', 'from', 'to', 'borrowed', 'returned'];
+
     // Relationship to book
     public function book()
     {
         return $this->belongsTo(Book::class, 'book_id');
+    }
+
+    // Relationship to user
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
