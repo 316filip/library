0962fa8889c301885e3dee8067e17102c6646bdb
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-08 10:08:52 +0100

    Design improvements

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="cs">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,25 @@
     <title>Stránka nenalezena</title>
     <link rel="shortcut icon" href="{{ asset('/img/icon.svg') }}" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <style>
+        .err404 {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            width: 100%;
+            transform: translateX(-50%) translateY(-50%) perspective(40em) rotateX(-20deg) rotateY(-10deg);
+        }
+    </style>
 </head>
+
 <body>
-    <img class="fixed -translate-x-1/2 bottom-0 left-1/2 h-2/5" src="{{ asset('/img/404.svg') }}" alt="Ilustrační obrázek">
-    <div class="fixed w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <h1 class="text-center text-8xl font-mono oldstyle-nums slashed-zero">404</h1>
+    <img class="fixed -translate-x-1/2 bottom-0 left-1/2 h-2/5" src="{{ asset('/img/404.svg') }}"
+        alt="Ilustrační obrázek">
+    <div class="err404">
+        <h1 class="text-center text-9xl font-mono oldstyle-nums slashed-zero">404</h1>
         <p class="text-center font-mono">Stránka nebyla nalezena.</p>
     </div>
 </body>
+
 </html>
