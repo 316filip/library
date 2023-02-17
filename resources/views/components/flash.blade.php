8fb6ad4ffbb96d041126c64a752d0562138347f0
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-17 21:23:59 +0100

    Bug fix

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -1,7 +1,7 @@
 {{-- If there is a flash message to display, this component will do it --}}
 @if (session()->has('message'))
     <div id="flash"
-        class="fixed w-2/3 sm:w-1/2 md:w-1/3 -top-28 -top-0 left-1/2 transform -translate-x-1/2 mt-5 px-10 py-3 bg-{{ $color_bg }} text-{{ $color_text }} shadow-lg rounded-lg z-40 transition-all">
+        class="fixed w-2/3 sm:w-1/2 md:w-1/3 -top-28 !-top-0 left-1/2 transform -translate-x-1/2 mt-5 px-10 py-3 bg-{{ $color_bg }} text-{{ $color_text }} shadow-lg rounded-lg z-40 transition-all">
         <p class="text-center">
             {{ session('message') }}
             @if (session()->has('link'))
@@ -13,7 +13,7 @@
     <script>
         // Hide the flash message after 6 seconds
         setTimeout(() => {
-            $('#flash').removeClass('-top-0');
+            $('#flash').removeClass('!-top-0');
         }, 6000);
     </script>
 @endif
