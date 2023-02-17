6d25339056a743416d88ac0985d4bd710a485ab3
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-17 21:21:15 +0100

    New mailable

---
+++ app/Mail/ExtensionConfirmation.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Booking;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ExtensionConfirmation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $booking;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Booking $booking)
+    {
+        $this->booking = $booking;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Potvrzení o prodloužení Vaší rezervace',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.extend',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
---
+++ resources/views/emails/extend.blade.php
@@ -0,0 +1,47 @@
+<html style="background-color: #e0f2fe;">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=">
+</head>
+
+<body
+    style='padding: 1rem 0 1rem 0; background-color: #e0f2fe; font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";'>
+    <center>
+        <table
+            style='background-color: white; max-width: 30rem; border-spacing: 1rem; text-align: center; border-radius: .5rem;'>
+            <tr>
+                <td>
+                    <h1 style="margin: 0;">Vaše rezervace byla prodloužena</h1>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p>Dobrý den,<br>právě jsme prodloužili lhůtu na vrácení Vámi vypůjčené knihy
+                        {{ $booking->book->title }} o jeden měsíc. Přejeme příjemné čtení a prosíme o včasné navrácení
+                        knihy.</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p>Kód rezervace: <strong>{{ $booking->code }}</strong></p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <center>
+                        <a href="{{ url('/kniha/' . $booking->book->id) }}"
+                            style='display: inline-block; padding: .5rem 1rem; background-color: #facc15; border-radius: .5rem; box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1); color: black; text-decoration: none;'>Zobrazit
+                            knihu</a>
+                    </center>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p>S pozdravem,<br>Vaše knihovna</p>
+                </td>
+            </tr>
+        </table>
+    </center>
+</body>
+
+</html>
