ecc65e22354a9bc0667a8e4474be5a36507af929
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-25 08:52:38 +0100

    Improved category page

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,7 +1,9 @@
 <x-layout>
     <h1 class="text-4xl text-center font-bold mb-5">
-        Kategorie {{ $category->name }}
+        {{ $category->name }}
     </h1>
+    <p class="text-center mb-5">{{ $category->description }}</p>
+    <x-Manage type="category" :identifier="[$category->id, $category->slug]"></x-Manage>
     <x-Deck type="work">
         @foreach ($assignments as $assignment)
             <x-Card type="work" :data="$assignment->work" number="0" more="0" placement="away" />
