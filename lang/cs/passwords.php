133206dc4c9f7ef2a7e9788b0c3ee7e210edcba7
filip316 <110289851+filip316@users.noreply.github.com>
2023-02-28 21:52:14 +0100

    Updated translations

---
+++ lang/cs/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Vaše heslo bylo resetováno!',
+    'sent' => 'Zaslali jsme Vám email s instrukcemi pro reset hesla!',
+    'throttled' => 'Prosím, počkejte, a zkuste to znovu.',
+    'token' => 'Váš odkaz není funkční.',
+    'user' => 'Nemohli jsme najít uživatele s touto emailovou adresou.',
+
+];
