047cc59e9cdea1c83fb7c45a104a7ffafdb120e8
filip316 <110289851+filip316@users.noreply.github.com>
2023-03-03 07:08:16 +0100

    Bug fix

--- app/View/Components/Card.php
+++ app/View/Components/Card.php
@@ -21,8 +21,8 @@ class Card extends Component
 
         $this->type = $type;
         $this->values = $data;
-        $this->classes = (($number > 3 && ($type == 'book' || $type == 'work' || $type == 'booking') || $number > 5) ? 'hidden' : 'block') . ($number > 5 ? ' min-[470px]:hidden' : ' min-[470px]:block') . ($number > 5 ? ' md:hidden' : ' md:block') . ($number > 7 ? ' lg:hidden' : ' lg:block') . ($number > 4 ? ' xl:hidden' : ' xl:block') . ($number > 5 ? ' 2xl:hidden' : ' 2xl:block');
-        $this->overlay = (($number != 3 && ($type == 'book' || $type == 'work' || $type == 'booking') || $number != 5) ? ' hidden' : ' block') . ($number != 5 ? ' min-[470px]:hidden' : ' min-[470px]:block') . ($number != 5 ? ' md:hidden' : ' md:block') . ($number != 7 ? ' lg:hidden' : ' lg:block') . ($number != 4 ? ' xl:hidden' : ' xl:block') . ($number != 5 ? ' 2xl:hidden' : ' 2xl:block');
+        $this->classes = (($number > 3 && ($type == 'book' || $type == 'work' || $type == 'booking') || $number > 5 && ($type == 'author' || $type == 'user')) ? 'hidden' : 'block') . ($number > 5 ? ' min-[470px]:hidden' : ' min-[470px]:block') . ($number > 5 ? ' md:hidden' : ' md:block') . ($number > 7 ? ' lg:hidden' : ' lg:block') . ($number > 4 ? ' xl:hidden' : ' xl:block') . ($number > 5 ? ' 2xl:hidden' : ' 2xl:block');
+        $this->overlay = (($number != 3 && ($type == 'book' || $type == 'work' || $type == 'booking') || $number != 5 && ($type == 'author' || $type == 'user')) ? ' hidden' : ' block') . ($number != 5 ? ' min-[470px]:hidden' : ' min-[470px]:block') . ($number != 5 ? ' md:hidden' : ' md:block') . ($number != 7 ? ' lg:hidden' : ' lg:block') . ($number != 4 ? ' xl:hidden' : ' xl:block') . ($number != 5 ? ' 2xl:hidden' : ' 2xl:block');
         $this->filter = $filter;
         $this->query = ($query == '') ? '' : '&query=' . $query;
         $this->in = ($filter == 'search') ? '&in=' . $type : '';
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -36,7 +36,8 @@
             </div>
         @elseif ($type == 'booking')
             {{-- When showing a booking, display booking details --}}
-            <div class="w-full place-self-end flex items-center gap-2 border border-slate rounded-lg p-2 shadow-inner mt-3">
+            <div
+                class="w-full place-self-end flex items-center gap-2 border border-slate rounded-lg p-2 shadow-inner mt-3">
                 <div class="basis-full">
                     @if (auth()->user()->librarian)
                         <p class="text-slate-500 text-sm">
