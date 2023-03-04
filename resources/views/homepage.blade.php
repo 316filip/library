70ec1f4182bf1e305c2a6458b97cd3c4f7b928b0
filip316 <110289851+filip316@users.noreply.github.com>
2023-03-04 21:31:48 +0100

    New content

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,3 +1,18 @@
 <x-layout>
-    <h1 class="text-4xl text-center font-bold">Knihovna</h1>
+    <div class="h-80 mb-8">
+        <div class="absolute w-full h-80 left-0">
+            <div
+                class="relative h-full bg-gradient-to-b from-yellow-100 to-yellow-200 flex items-center justify-center -top-6">
+                <h1 class="text-6xl text-center font-bold">Knihovna</h1>
+            </div>
+            <div class="w-full -translate-y-3/4 h-20 bg-contain bg-repeat-x bg-center"
+                style="background-image: url(/img/devider.svg)">
+            </div>
+        </div>
+    </div>
+    <div>
+        <h1 class="text-4xl text-center font-bold mb-5">Vítejte!</h1>
+        <p class="text-center">Vytvořte si účet v naší knihovně a otevřte si dveře do bezplatné knihony plné titulů,
+            které Vás uchvátí.</p>
+    </div>
 </x-layout>
