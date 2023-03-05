4a26f92711923bae9092bf89c887db2e379e87fc
filip316 <110289851+filip316@users.noreply.github.com>
2023-03-05 16:55:53 +0100

    Added new content

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -12,7 +12,50 @@
     </div>
     <div>
         <h1 class="text-4xl text-center font-bold mb-5">Vítejte!</h1>
-        <p class="text-center">Vytvořte si účet v naší knihovně a otevřte si dveře do bezplatné knihony plné titulů,
+        <p class="text-center mb-5">Vytvořte si účet v naší knihovně a otevřte si dveře do bezplatné knihony plné titulů,
             které Vás uchvátí.</p>
+        <p class="text-center py-2 mb-5">
+            <a href="#"
+                class="px-3 py-2 shadow bg-yellow-400 hover:bg-amber-400 transition rounded-lg">Procházet</a>
+        </p>
+        <p class="text-center mb-5">Pokud hledáte něco konkrétního, můžete to zkusit vyhledat.</p>
+        <div class="flex justify-center mb-5">
+            <button class="px-3 py-2 shadow bg-sky-100 hover:bg-sky-200 transition rounded-lg"
+                onclick="showSearchBar()">Hledat</button>
+        </div>
+        <h2 class="text-2xl text-center font-bold mb-5">Často kladené otázky</h2>
+        <div class="flex justify-center">
+            <div class="w-full max-w-4xl grid gap-3">
+                <div class="border border-slate-200 rounded-lg px-4">
+                    <div class="py-4 cursor-pointer" onclick="faq(1)">Jak se mohu stát členem? <i
+                            class="fa-solid fa-caret-right transition" id="faq1-toggle"></i></div>
+                    <div class="pt-4 border-t border-slate-200 py-4" id="faq1" style="display: none;">
+                        <p>Abyste si mohli začít půjčovat knihy v naší knihovně, stačí si vytvořit účet na našem webu.
+                            Při Vaší první osobní návštěvě Vám dáme Váš čtenářský průkaz. Jeho cena je 50 Kč.</p>
+                    </div>
+                </div>
+                <div class="border border-slate-200 rounded-lg px-4">
+                    <div class="py-4 cursor-pointer" onclick="faq(2)">Jaká je cena za zapůjčení knihy? <i
+                            class="fa-solid fa-caret-right transition" id="faq2-toggle"></i></div>
+                    <div class="pt-4 border-t border-slate-200 py-4" id="faq2" style="display: none;">
+                        <p>Zapůjčení knihy je bezplatné. Platí se pouze za vydání čtenářského průkazu a případná pokuta
+                            za pozdní vrácení knihy.</p>
+                    </div>
+                </div>
+                <div class="border border-slate-200 rounded-lg px-4">
+                    <div class="py-4 cursor-pointer" onclick="faq(3)">Kolik knih mohu mít půjčených současně? <i
+                            class="fa-solid fa-caret-right transition" id="faq3-toggle"></i></div>
+                    <div class="pt-4 border-t border-slate-200 py-4" id="faq3" style="display: none;">
+                        <p>V jednu chvíli můžete mít rezervovaných až pět knih.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <script>
+        function faq(num) {
+            $('#faq' + num).slideToggle();
+            $('#faq' + num + '-toggle').toggleClass('rotate-90');
+        }
+    </script>
 </x-layout>
